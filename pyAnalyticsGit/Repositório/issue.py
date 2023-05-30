487e0b06a673fcbf812fed804cbc351b0fa2ab7b
Tiago1604 <202028973@aluno.unb.br>
2023-05-30 17:35:20 -0300

    Atualização da Classe issue para abrir relatório.

--- pyAnalyticsGit/Repositório/issue.py
+++ pyAnalyticsGit/Repositório/issue.py
@@ -8,40 +8,57 @@ class Issue:
         self.username = username
         self.reponame = reponame
 
-        url = f'https://api.github.com/repos/{username}/{reponame}/issues?state=all'
+        page = 1
+        per_page = 30
+        self.all_issues = []
 
-        response = requests.get(url)
+        while True:           
+            response = requests.get(f'https://api.github.com/repos/{username}/{reponame}/issues?state=all&page={page}&per_page={per_page}')
 
-        if response.status_code == 200:
-            self.issues = response.json()
-            print("conexão estabelecida\n")
-        else:
-            print(f'Falha ao obter os detalhes do repositório {reponame}.')
-            exit()
+            if response.status_code == 200:
+                self.issues = response.json()
+                self.all_issues.extend(self.issues)
+                #print("conexão estabelecida\n")
+
+                if len(self.issues) < per_page:
+                    break
+                else:
+                    page += 1
+
+            else:
+                print(f'Falha ao obter os detalhes do repositório {reponame}.')
+                print(f'StatusCode: {response.status_code}')
+                break
 
     def listar_issue(self):
-        for issue in self.issues:
-            print(f'Título: {issue["title"]}')
-            print(f'Estado: {issue["state"]}')
-            print(f'Número: {issue["number"]}')
+        arq = open("Issues.md","w+")
+        for issue in self.all_issues:
+            arq.write(f'- Título: {issue["title"]}\n')
+            arq.write(f'- Estado: {issue["state"]}\n')
+            arq.write(f'- Número: {issue["number"]}\n')
             labels = []
             for label in issue["labels"]:
                 labels.append(label["name"])
-            print("Labels:",", ".join(labels))
-            print('---------------------')
+            #arq.write("Labels:",", ".join(labels))
+            arq.write(f'- Labels: {", ".join(labels)}\n')
+            arq.write('---------------------\n')
 
     def listar_issue_label(self,label):
+        arq = open(f'issues_{label}.md',"w+")
+        arq.write(f'### Todas as issues de **{label}:**\n\n')
         c = 0
-        for issue in self.issues:
+        for issue in self.all_issues:
             for labels in issue["labels"]:
                 if labels["name"] == label:
-                    print(f'Título: {issue["title"]}')
-                    print(f'Estado: {issue["state"]}')
-                    print(f'Número: {issue["number"]}')
-                    print('---------------------')
+                    arq.write(f'Título: {issue["title"]}\n')
+                    arq.write(f'Estado: {issue["state"]}\n')
+                    arq.write(f'Número: {issue["number"]}\n')
+                    arq.write('---------------------\n')
                     c = c + 1
         if c == 0 :
-            print("Label não encontrada")              
+            print("Label não encontrada")
+        else:
+            print(c)              
                     
 
 
@@ -52,4 +69,5 @@ issue2 = Issue()
 #issue1.listar_issue()
 
 issue2.connect("fga-eps-mds","2023.1-PyAnalyticsGit")
-issue2.listar_issue()
+issue2.listar_issue_label("documentation")
+#issue2.listar_issue()
