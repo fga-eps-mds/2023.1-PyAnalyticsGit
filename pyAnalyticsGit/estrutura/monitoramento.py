7dab2812c34d5bb74c167d59d18d99def6791a02
JeffersonSenaa <jeffersonsena12144@gmail.com>
2023-05-31 21:34:14 -0300

    Teste de automatização

--- pyAnalyticsGit/estrutura/monitoramento.py
+++ pyAnalyticsGit/estrutura/monitoramento.py
@@ -2,7 +2,7 @@ class monitoramento:
 
   def rastreamento_git(self):
     """Quando algum commit for realizado este metodo é chamado. """
-    print("Função chamada com sucesso, commit realizado")
+    print("Teste de automatização realizado com sucesso!")
     pass
 
 obj = monitoramento()
