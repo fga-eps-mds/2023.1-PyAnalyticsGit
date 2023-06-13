a7a84b2918fd0696b05e39b7b7dbb5f817710f28
JeffersonSenaa <jeffersonsena12144@gmail.com>
2023-06-12 22:31:13 -0300

    Implementação do método que verifica o arquivo .git do repositório.

--- pyAnalyticsGit/estrutura/mod_biblioteca.py
+++ pyAnalyticsGit/estrutura/mod_biblioteca.py
@@ -1,11 +1,15 @@
 import subprocess
+import os
 
 class mod_biblioteca:
 
+    caminho_repositorio = None
+
     @staticmethod
     def automatiza_commit():
-        diretorio_hooks = "/home/jefferson/unb/mds/projeto_mds/2023.1-PyAnalyticsGit/.git/hooks"
+        diretorio_hooks = f'{mod_biblioteca.caminho_repositorio}/hooks'
 
+        #Path onde a biblioteca foi instalada
         comando_post_commit = '''#!/bin/sh
             /usr/bin/python3 /home/jefferson/unb/mds/projeto_mds/2023.1-PyAnalyticsGit/pyAnalyticsGit/estrutura/monitoramento.py '''
 
@@ -20,6 +24,30 @@ class mod_biblioteca:
 
         subprocess.run(['chmod', '+x', diretorio_post_commit])
 
-obj = mod_biblioteca()
-obj.automatiza_commit()
+    @staticmethod
+    def verifica_arquivo_git():
+        #Verifica se o arquivo .git esta no path e salva o caminho
+        diretorio_atual = os.getcwd()   
+        mod_biblioteca.caminho_repositorio = os.path.join(diretorio_atual, ".git")
+        
+        if os.path.exists(mod_biblioteca.caminho_repositorio):
+            print("Caminho do repositório:", mod_biblioteca.caminho_repositorio)
+            return mod_biblioteca.caminho_repositorio
+        else:
+            print("Arquivo .git do repositório não encontrado.")
+        return None
+    
+    @staticmethod
+    def acha_repositório():
+        resultado = subprocess.run(['readlink', '-f',  __file__], capture_output=True, text=True)
 
+        if resultado.returncode == 0:
+            caminho_repositorio = resultado.stdout.strip()  #variavel chave
+            #diretorio_atual = caminho_repositorio.rpartition('/')[0]
+            print("Caminho do repositório:", caminho_repositorio) 
+        return caminho_repositorio
+    
+
+obj = mod_biblioteca()
+obj.acha_repositório()
+obj.verifica_arquivo_git()
