4474c391b43b3a6db908073e64aca41e998d8094
Mateus Levy <levymateus06@Gmail.com>
2023-07-11 15:55:41 -0300

    adding test_criar_grafico_commit()

--- pyAnalyticsGit/analyticsgit/test_images.py
+++ pyAnalyticsGit/analyticsgit/test_images.py
@@ -18,3 +18,11 @@ def test_gerar_nuvem_commits():
         assert os.path.exists(os.path.join(caminho_pasta,grafico_nuvem))
     except Exception as e :
         pytest.fail(str(e))
+
+def test_criar_grafico_commit():
+    obj = Commits()
+    try:
+        obj.criar_grafico_commit()
+        assert os.path.exists(os.path.join(caminho_pasta,grafico_nuvem))
+    except Exception as e:
+        pytest.fail(str(e))
