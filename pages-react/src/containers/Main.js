f854a5d201febdee2c66ab83a1bf94eca64ebae7
Mateus Levy <levymateus06@Gmail.com>
2023-07-11 00:41:48 -0300

    fixing routes

--- pages-react/src/containers/Main.js
+++ pages-react/src/containers/Main.js
@@ -4,16 +4,12 @@ import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
-import Opensource from "../pages/opensource/Opensource";
-import Contact from "../pages/contact/ContactComponent";
-import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
-
   render() {
-    console.log(this.props)
+    console.log(this.props);
     if (settings.isSplash) {
       return (
         <div>
@@ -44,27 +40,9 @@ export default class Main extends Component {
                 )}
               />
               <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
                 path="/splash"
                 render={(props) => (
                   <Splash {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
                 )}
               />
               <Route
@@ -102,33 +80,6 @@ export default class Main extends Component {
                 path="/education"
                 render={(props) => (
                   <Education {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              {/* <Route
-							path="/splash"
-							render={(props) => (
-								<Splash
-									{...props}
-									theme={this.props.theme}
-								/>
-							)}
-						/> */}
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
                 )}
               />
             </Switch>
